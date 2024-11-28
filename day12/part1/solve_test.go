add2459a592ec0e18f26f81cc83a2ff30dddf9ec
sqrtxander <xanderjames128@gmail.com>
2024-11-28 14:36:28 +1100

    day12 complete

---
+++ day12/part1/solve_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+var INPUT string = `
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5
+`[1:]
+
+var EXPECTED int = 6
+
+func TestSolve(t *testing.T) {
+	actual := solve(INPUT)
+	if actual != EXPECTED {
+		t.Fatalf("Expected %d got %d\n", EXPECTED, actual)
+	}
+}
---
+++ day12/part2/solve.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"aoc2017/utils"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+func solve(s string) int {
+	s = strings.TrimSpace(s)
+	lines := strings.Split(s, "\n")
+
+	communicates := map[int][]int{}
+
+	for _, line := range lines {
+		id, direct, _ := strings.Cut(line, " <-> ")
+		communicates[utils.HandledAtoi(id)] = utils.Map(strings.Split(direct, ", "), utils.HandledAtoi)
+	}
+
+	seen := map[int]bool{}
+	queue := utils.Queue[int]{}
+	count := 0
+
+	for len(communicates) > 0 {
+		count++
+		for k := range communicates {
+			queue = queue.Push(k)
+			break
+		}
+		for len(queue) > 0 {
+			var q int
+			queue, q = queue.Pop()
+			for _, direct := range communicates[q] {
+				if seen[direct] {
+					continue
+				}
+				seen[direct] = true
+				queue = queue.Push(direct)
+			}
+			delete(communicates, q)
+		}
+	}
+	return count
+}
+
+func main() {
+	var inputPath string
+	if len(os.Args) > 1 {
+		inputPath = os.Args[1]
+	} else {
+		_, currentFilePath, _, _ := runtime.Caller(0)
+		dir := filepath.Dir(currentFilePath)
+		dir = filepath.Dir(dir)
+		inputPath = filepath.Join(dir, "input.in")
+	}
+	contents, err := os.ReadFile(inputPath)
+	if err != nil {
+		log.Fatalf("Error reading file %s:\n%v\n", inputPath, err)
+		return
+	}
+	fmt.Println(solve(string(contents)))
+}
