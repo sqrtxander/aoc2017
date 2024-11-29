4a14bacf0ee7dc9214b976cbc9e87fc18f4a2dee
sqrtxander <xanderjames128@gmail.com>
2024-11-30 01:18:34 +1100

    day24 complete

---
+++ day24/part1/solve.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"aoc2017/utils"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"slices"
+	"strings"
+)
+
+type bridge struct {
+	ports     []*utils.Pair[int, int]
+	nextMatch int
+	strength  int
+}
+
+func parsePort(line string) *utils.Pair[int, int] {
+	l, r, _ := strings.Cut(line, "/")
+	return &utils.Pair[int, int]{
+		K: utils.HandledAtoi(l),
+		V: utils.HandledAtoi(r),
+	}
+}
+
+func solve(s string) int {
+	s = strings.TrimSpace(s)
+	lines := strings.Split(s, "\n")
+	allPorts := utils.Map(lines, parsePort)
+	queue := utils.Queue[*bridge]{}
+	for _, port := range allPorts {
+		if port.K == 0 {
+			queue = queue.Push(&bridge{
+				ports:     []*utils.Pair[int, int]{port},
+				nextMatch: port.V,
+				strength:  port.V,
+			})
+		} else if port.V == 0 {
+			queue = queue.Push(&bridge{
+				ports:     []*utils.Pair[int, int]{port},
+				nextMatch: port.K,
+				strength:  port.K,
+			})
+		}
+	}
+	maxStrength := 0
+	for len(queue) > 0 {
+		var b *bridge
+		queue, b = queue.Pop()
+		maxStrength = max(maxStrength, b.strength)
+		match := false
+		for _, port := range allPorts {
+			if slices.Contains(b.ports, port) {
+				continue
+			}
+			if port.K != b.nextMatch && port.V != b.nextMatch {
+				continue
+			}
+			if match {
+				_ = match
+			}
+			match = true
+			nextMatch := port.K
+			if port.K == b.nextMatch {
+				nextMatch = port.V
+			}
+			newPorts := append(slices.Clone(b.ports), port)
+			queue = queue.Push(&bridge{
+				ports:     newPorts,
+				nextMatch: nextMatch,
+				strength:  b.strength + port.K + port.V,
+			})
+		}
+	}
+
+	return maxStrength
+}
+
+func main() {
+	var inputPath string
+	if len(os.Args) > 1 {
+		inputPath = os.Args[1]
+	} else {
+		_, currentFilePath, _, _ := runtime.Caller(0)
+		dir := filepath.Dir(currentFilePath)
+		dir = filepath.Dir(dir)
+		inputPath = filepath.Join(dir, "input.in")
+	}
+	contents, err := os.ReadFile(inputPath)
+	if err != nil {
+		log.Fatalf("Error reading file %s:\n%v\n", inputPath, err)
+		return
+	}
+	fmt.Println(solve(string(contents)))
+}
---
+++ day24/part1/solve_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+var INPUT string = `
+0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10
+`[1:]
+
+var EXPECTED int = 31
+
+func TestSolve(t *testing.T) {
+	actual := solve(INPUT)
+	if actual != EXPECTED {
+		t.Fatalf("Expected %d got %d\n", EXPECTED, actual)
+	}
+}
